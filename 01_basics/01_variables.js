143bb5d634da4986655042a4642479f48c3a52b2
Akchat25 <138615911+Akchat25@users.noreply.github.com>
2026-07-06 20:40:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =14456
+let accountEmail ="akchat@gmail.com"
+var accountPassword="1234"
+accountCity="jaipur"
+
+//accountId =2// ye nhi save ho skta//
+accountEmail ="rashi@gmail.com"
+accountPassword="1357"
+accountCity="blr"
+
+console.log(accountId)
+
+//please do not use var because of issue in block scope and functional scope//
+
+console.table([accountId,accountEmail,accountPassword,accountCity ])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+console.log("AKCHAT")
+
+//NUMBER=> 2 TO POWER 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>
+//symbol => unique
+
+console.log(typeof age)
